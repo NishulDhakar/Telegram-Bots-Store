0b2a3812fd82d20cad86700bf1b40197ab279c5f
nishuldhakar <nishuldhakar.se@gmail.com>
2026-06-08 22:41:42 +0530

    update projects

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -17,7 +17,7 @@ export const Bots: Project[] = [
     description: "A professional Telegram bot designed to automatically forward messages from channels or chats to specified destinations with support for customizable message filtering, keyword rules, and template formatting.",
     image: "/images/forward.jpg",
     tags: [],
-    liveUrl: "https://t.me/Auto_Forwarder_Official_Bot?start=_tgr_pjYBnE0zNGZl",
+    liveUrl: "https://t.me/Auto_Forwarder_Official_Bot",
     githubUrl: "https://github.com/NishulDhakar/Auto-forward-bot"
   },
   {
@@ -27,7 +27,7 @@ export const Bots: Project[] = [
     description: "A specialized Telegram bot that automatically processes and approves joining requests for channels or groups, enabling instant and friction-free user onboarding.",
     image: "/images/approve.jpg",
     tags: [],
-    liveUrl: "https://t.me/Auto_request_accept_pro_bot?start=_tgr_C4GulsxkZGZl",
+    liveUrl: "https://t.me/Auto_request_accept_pro_bot",
     githubUrl: "https://github.com/NishulDhakar/Auto-approve-bot"
   },
   {
@@ -37,7 +37,7 @@ export const Bots: Project[] = [
     description: "An interactive Telegram bot that automatically appends pre-configured emoji reactions to new messages in channels or groups to drive higher engagement and activity.",
     image: "/images/reaction.jpg",
     tags: [],
-    liveUrl: "https://t.me/ReactionSavvyBot?start=_tgr_AzA7VAhiMWE1",
+    liveUrl: "https://t.me/ReactionSavvyBot",
     githubUrl: "https://github.com/NishulDhakar/Auto-Reaction-Bot"
   }
 ];
